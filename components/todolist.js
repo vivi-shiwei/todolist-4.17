650e6b0fb06e39e831570155bc1b51c108396f91
薇薇 <2546710985@qq.com>
2020-04-17 10:41:38 +0800

    Initial commit

---
+++ components/todolist.js
@@ -0,0 +1,179 @@
+import React, {Component} from 'react';
+
+
+class Todolist extends Component {
+    constructor(props) {
+        super(props);
+        this.textInput = React.createRef();
+        this.state={
+            list:[],
+            finishList:[]
+        };
+            }
+            locsetItem=(key, value)=>{
+                localStorage.setItem(key,JSON.stringify(value));
+            };
+            locgetItem=(key)=>{
+                return JSON.parse(localStorage.getItem(key));
+            };
+            componentDidMount() {
+                let locTodolist = this.locgetItem("Todolist");
+                if(locTodolist){
+                    this.setState({
+                        finishList:locTodolist
+            })
+        }
+    }
+
+    addData=()=>{
+        if(this.textInput.current.value===""){
+            return false;
+        }
+        else {
+            var tempList = this.state.finishList;
+            tempList.push(this.textInput.current.value);
+            this.textInput.current.value="";
+            this.setState({
+                list:tempList
+            })
+            this.locsetItem("Todolist",tempList);
+        }
+    };
+    removeData=(key)=>{
+        var tempList= this.state.finishList;
+        tempList.splice(key,1);
+        this.setState({
+            list:tempList
+        })
+        this.locsetItem("Todolist",tempList)
+    };
+
+    render() {
+        return(
+            <div id="App">
+                <div id="todolist">
+                    <header>
+                        <h1>React Todolist</h1>
+                    </header>
+                    <div className="center">
+                        <div id="inpTodolist">
+                            <input className="txtTodolist" ref={this.textInput}/>
+                        </div>
+                        <button className="butAdd" onClick={this.addData}>Submit</button>
+                    </div>
+                    <ul>
+                        {
+                            this.state.finishList.map((value,key)=> {
+                                return(
+                                    <li key={key}>
+                                        <div>
+                                            <button className="butDel" onClick={this.removeData.bind(this,key)}>remove</button>
+                                            <p>{value}</p>
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+                <style global jsx>{`
+                *{
+                    margin: 0;
+                    padding: 0;
+                    list-style: none;
+                }
+                    h1{
+                        text-align: center;
+                        line-height: 88px;
+                    }
+                    header{
+                        height: 90px;
+                    }
+                    li{
+                        height: 60px;
+                    }
+                    li>div{
+                        height: 48px;
+                        margin: auto;
+                        border-bottom: #F1F1F1 solid 2px;
+                    }
+                    p{
+                        margin-left: 100px;
+                        font-size: 20px;
+                        line-height: 48px;
+                    }
+                    button{
+                        border-radius: 4px;
+                    }
+                    #App{
+                        margin: 0 auto;
+                        width: 800px;
+                        height: 900px;
+                        position: relative;
+                        border-top: #40C1FD solid 6px ;
+                        border-left: #40C1FD solid 6px;
+                        border-right: #40C1FD solid 6px;
+                    }
+
+                    #todolist{
+                        height: 860px;
+                        width: 660px;
+                        position: absolute;
+                        left: 0;
+                        right: 0;
+                        top: 0;
+                        bottom: 0;
+                        margin: auto;
+                    }
+                    .center{
+                        height: 40px;
+                        width: 500px;
+                        margin: auto;
+                    }
+                    #inpTodolist{
+                        width: 80%;
+                        height: 86%;
+                        border-bottom: 3px solid #40C1FD;
+                        border-top: 3px solid #40C1FD;
+                        float: left;
+                    }
+                    .txtTodolist{
+                        font-size: 20px;
+                        height: 33px;
+                        width: 300px;
+                        outline: none;
+                        border: none;
+                    }
+                    .butAdd{
+                        color: #40C1FD;
+                        font-size: 16px;
+                        background-color: white;
+                        margin-left: 0;
+                        width: 15%;
+                        height: 100%;
+                        border: #40C1FD solid 3px;
+                        float: right;
+                    }
+                    ul{
+                        width: 450px;
+                        height: 200px;
+                        margin: 0 auto;
+                        margin-top: 20px;
+                    }
+                    .butDel{
+                        color: #D2A5B4;
+                        font-size: 16px;
+                        background-color: white;
+                        /*margin-left: 0;*/
+                        width: 75px;
+                        height: 40px;
+                        float: left;
+                        border: #D2A5B4 solid 3px;
+                    }
+                    `}</style>
+            </div>
+        );
+    }
+}
+
+export default Todolist;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import Todolist from '../components/todolist';
+
+export default function Index(){
+    return (
+        <div id="root">
+            <Todolist />
+        </div>
+    )
+}
